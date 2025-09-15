00ec25cb403e9b3ff73e62b0fc8696b9f11c7d27
IT24102544 <it24102544@my.sliit.lk>
2025-09-15 21:08:49 +0530

    Lab 06

---
+++ Lab Sheet 06/IT24102544.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\User\\Desktop\\IT24102544")
+getwd()
+
+#Question 01
+#i)
+#Binomial Theorum 
+#ii)
+#Here, random variabl X has binomial distribution with n=50 and p=0.85
+1-pbinom(46,50,0.85, lower.tail = TRUE)
+
+#Question 02
+#i)
+#X=number of calls in one hour
+#ii)
+#Poisson Distribution
+#iii)
+#Here, random variable X has poisson distribustion with lambda=12
+dpois(15,12)
+
+
